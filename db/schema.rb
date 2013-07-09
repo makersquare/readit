9ed4006c010f3542a255b82ba7986a9b86731524
MakerSquare Student <students@themakersquare.com>
2013-07-09 08:59:41 -0500

    Add migration to add title to Link model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130709135512) do
+ActiveRecord::Schema.define(:version => 20130709135829) do
 
   create_table "links", :force => true do |t|
     t.integer  "user_id"
     t.string   "url"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "title"
   end
 
   create_table "users", :force => true do |t|
