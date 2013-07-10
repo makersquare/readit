331e49d6ad0ecd9a1118bd2cdcaa51264e78e947
MakerSquare Student <students@themakersquare.com>
2013-07-10 09:05:54 -0500

    Paginate links.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def index
-    @links = Link.all
+    params[:per_page] ||= 25
+    params[:page]     ||= 1
+
+    @links = Link.order('created_at DESC').page(params[:page]).per_page(params[:per_page])
   end
 end
